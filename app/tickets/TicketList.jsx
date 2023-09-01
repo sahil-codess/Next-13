61ed9449b83eda584c1610b3854a7f6b35c25021
Sahilkhan <sahilkhanahmadzai4@gmail.com>
2023-09-01 22:44:09 +0430

    added dynamic segments in next which is show the dynamic data and learned about the structure of creating it just like it was in next's previous versions

--- app/tickets/TicketList.jsx
+++ app/tickets/TicketList.jsx
@@ -1,8 +1,10 @@
+import Link from "next/link";
+
 async function getTickets() {
-  const res = await fetch("http://localhost:4000/tickets" , {
+  const res = await fetch("http://localhost:4000/tickets", {
     next: {
-        revalidate: 0
-    }
+      revalidate: 0,
+    },
   });
 
   return res.json();
@@ -14,11 +16,13 @@ export default async function TicketList() {
     <>
       {tickets.map((ticket) => (
         <div key={ticket.id} className="card my-5">
+          <Link href={`/tickets/${ticket.id}`}>
             <h3>{ticket.title}</h3>
             <p>{ticket.body.slice(0, 200)}...</p>
             <div className={`pill ${ticket.priority}`}>
-            {ticket.priority} priority
+              {ticket.priority} priority
             </div>
+          </Link>
         </div>
       ))}
       {tickets.length === 0 && (
