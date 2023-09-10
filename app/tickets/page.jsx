a5cf34de36d57dd8ad73183f6c229643dbc3c752
Sahilkhan <sahilkhanahmadzai4@gmail.com>
2023-09-10 11:58:16 +0430

    learned fetching data with server components which every component in next is a server component

--- app/tickets/page.jsx
+++ app/tickets/page.jsx
@@ -1,5 +1,18 @@
+import TicketList from "./TicketList";
+
 const Tickets = () => {
-  return <div>Tickets</div>;
+  return (
+    <main>
+      <nav>
+        <div>
+          <h2>Tickets</h2>
+          <p><small>Currently open tickets</small></p>
+        </div>
+      </nav>
+
+      <TicketList />
+    </main>
+  )
 };
 
 export default Tickets;
