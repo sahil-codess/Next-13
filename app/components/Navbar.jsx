ee976c9558fa4b3a385ed77bf4cae649992e170a
Sahilkhan <sahilkhanahmadzai4@gmail.com>
2023-09-10 11:16:04 +0430

    added the logo using image component

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -1,8 +1,11 @@
 import Link from "next/link";
+import Image from "next/image";
+import Logo from './dojo-logo.png';
 
 const Navbar = () => {
   return (
     <nav>
+    <Image src={Logo} alt="Dogo Logo" width={70} quality={100} placeholder="blur" />
     <h1>Sahilkhan Helpdisk</h1>
       <Link href="/">Dashboard</Link>
       <Link href="/tickets">Tickets</Link>
